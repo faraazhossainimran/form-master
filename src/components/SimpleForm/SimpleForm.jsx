b4dcd1f4161e9218366b7dd699d30b1db789b634
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-21 23:43:35 +0600

    project setup

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,15 @@
+
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    )
+}
+
+export default SimpleForm
