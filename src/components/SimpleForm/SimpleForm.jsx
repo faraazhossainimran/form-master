bce22b3664fab12cb48188b963bcc37be605c55e
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-22 23:59:16 +0600

    simple form added

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,20 @@
 
 
 const SimpleForm = () => {
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log("form submitted");
+    }
     return (
         <div>
             <form>
-                <input type="text" />
+                <input type="text" name="name" />
                 <br/>
-                <input type="submit" value="Submit"/>
+                <input type="email" name="email"/>
+                <br/>
+                <input type="number" name="number"/>
+                <br/>
+                <input onSubmit={handleSubmit} type="submit" value="Submit"/>
             </form>
         </div>
     )
